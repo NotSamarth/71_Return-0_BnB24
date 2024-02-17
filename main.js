e5995a010b16c1bdafc1966f294d61e646e96173
NotSamarth <samarthvaishnav33@gmail.com>
2024-02-17 17:33:59 +0530

    first commit basic files added

---
+++ main.js
@@ -0,0 +1,13 @@
+import './style.css'
+import firebase  from 'firebase/app';
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBgTXIcy_Av3pWj1BYg18gxRAubIabDYbQ",
+    authDomain: "apnamed-774ec.firebaseapp.com",
+    projectId: "apnamed-774ec",
+    storageBucket: "apnamed-774ec.appspot.com",
+    messagingSenderId: "731208891868",
+    appId: "1:731208891868:web:456c3534fb35c2b91a6319",
+    measurementId: "G-X649HRWQ81"
+  };
